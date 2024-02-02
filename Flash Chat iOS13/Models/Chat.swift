70583f0561100742f56171a32ffa72b572ddaae8
Rpinafora <rafaelfora96@gmail.com>
2024-02-02 22:07:08 +0000

    News editions of flashChat

---
+++ Flash Chat iOS13/Models/Chat.swift
@@ -0,0 +1,15 @@
+//
+//  Chat.swift
+//  Flash Chat iOS13
+//
+//  Created by Rafael Martins Pina Fora on 11/03/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Chat{
+    let sender1: String
+    let sender2: String
+    
+}
